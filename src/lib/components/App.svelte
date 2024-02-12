ab579be8a93a87faff031cc5f6d28a939fb0a752
deepfelt <dgodesign77@gmail.com>
2024-02-12 09:43:37 -0500

    Removed navbar

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -7,20 +7,7 @@
   const expanded = writable(false)
 </script>
 
-<nav class="w-full h-auto p-5 text-2xl text-gray-400 bg-purple-800 flex">
-  <ul class="flex items-center gap-5 ">
-    <li>
-  <a href="{base}/">Home</a>
-  </li>
-  <li>
-    <button on:click={() => $expanded = !$expanded}>Lessons</button>
-  </li>
 
- 
-
-  </ul>
-
-</nav>
 <div class="w-full h-auto p-5 text-2xl text-purple-400 bg-gray-800">
   This is an HTML element inside <span class="bg-slate-200 text-purple-950 w-auto font-mono p-1 rounded-sm">src/components/App.svelte</span>
 </div>
